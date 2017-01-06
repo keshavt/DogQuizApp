aec6078b8c54ad393681ccb90e23d4012af22c36
keshavth@yahoo.com <keshavth@yahoo.com>
2017-01-05 23:41:03 -0800

    Add project files.

---
+++ DogQuiz/data/cats.js
@@ -0,0 +1,123 @@
+{
+    "quiz": {
+        "Id": 1,
+        "name": "Cats trivia",
+        "description": "Only CAT - trivia questions"
+    },
+    "config":{
+        "shuffleQuestions": true,
+        "showPager": false,
+        "allowBack": true,
+        "autoMove": true
+    },
+    "questions": [{
+        "Id": 3010,
+        "Name": "Normal adult dogs have how many teeth?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "24", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 3010, "Name": "38", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "42", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 3010, "Name": "32", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3011,
+        "Name": "Through what part of the body do dogs sweat?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "Mouth", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 3010, "Name": "Ears", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "Nose", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 3010, "Name": "Paws", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3012,
+        "Name": "What is the most common training command taught to dogs?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "Stay", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "Beg", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 3010, "Name": "Sit", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 3010, "Name": "Dance", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3013,
+        "Name": "Which dog breed has a black tongue?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "Husky", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "Labrador", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 3010, "Name": "Weimaraner", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 3010, "Name": "Chow chow", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3014,
+        "Name": "What is a dog’s most highly developed sense?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "Taste", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "Smell", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 3010, "Name": "Sight", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 3010, "Name": "Touch", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3015,
+        "Name": "Puppies are delivered how many weeks after conception?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "36", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "22", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 3010, "Name": "9", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 3010, "Name": "16", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3016,
+        "Name": "What is the favorite dog breed of the Queen of England?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "Corgi", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 3010, "Name": "Basenji", "IsAnswer": false},
+            { "Id": 1057, "QuestionId": 3010, "Name": "Poodle", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 3010, "Name": "Pomeranian", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3017,
+        "Name": "Which TV series had a dog named K9 who was also a robot?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "Full House", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 3010, "Name": "Star Trek", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "Doctor Who", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 3010, "Name": "Law & Order", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3018,
+        "Name": "Which dog breed is the smallest of them all?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "Dachshund", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "Shih tzu", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 3010, "Name": "Pomeranian", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 3010, "Name": "Chihuahua", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 3019,
+        "Name": "Which breed was once known as St. John’s Newfoundland?",
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 3010, "Name": "Newfoundland", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 3010, "Name": "Golden retriever", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 3010, "Name": "Labrador", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 3010, "Name": "Puli", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    }]
+}
---
+++ DogQuiz/data/dogs.js
@@ -0,0 +1,123 @@
+{
+    "quiz": {
+        "Id": 2,
+        "name": "Dogs trivia",
+        "description": "Only DOG - trivia questions"
+    },
+    "config":{
+        "shuffleQuestions": true,
+        "showPager": false,
+        "allowBack": true,
+        "autoMove": true
+    },
+    "questions": [{
+        "Id": 2010,
+        "Name": "Normal adult dogs have how many teeth?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "24", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 2010, "Name": "38", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "42", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 2010, "Name": "32", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2011,
+        "Name": "Through what part of the body do dogs sweat?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "Mouth", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 2010, "Name": "Ears", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "Nose", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 2010, "Name": "Paws", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2012,
+        "Name": "What is the most common training command taught to dogs?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "Stay", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "Beg", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 2010, "Name": "Sit", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 2010, "Name": "Dance", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2013,
+        "Name": "Which dog breed has a black tongue?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "Husky", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "Labrador", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 2010, "Name": "Weimaraner", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 2010, "Name": "Chow chow", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2014,
+        "Name": "What is a dog’s most highly developed sense?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "Taste", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "Smell", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 2010, "Name": "Sight", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 2010, "Name": "Touch", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2015,
+        "Name": "Puppies are delivered how many weeks after conception?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "36", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "22", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 2010, "Name": "9", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 2010, "Name": "16", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2016,
+        "Name": "What is the favorite dog breed of the Queen of England?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "Corgi", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 2010, "Name": "Basenji", "IsAnswer": false},
+            { "Id": 1057, "QuestionId": 2010, "Name": "Poodle", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 2010, "Name": "Pomeranian", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2017,
+        "Name": "Which TV series had a dog named K9 who was also a robot?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "Full House", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 2010, "Name": "Star Trek", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "Doctor Who", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 2010, "Name": "Law & Order", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2018,
+        "Name": "Which dog breed is the smallest of them all?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "Dachshund", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "Shih tzu", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 2010, "Name": "Pomeranian", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 2010, "Name": "Chihuahua", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 2019,
+        "Name": "Which breed was once known as St. John’s Newfoundland?",
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 2010, "Name": "Newfoundland", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 2010, "Name": "Golden retriever", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 2010, "Name": "Labrador", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 2010, "Name": "Puli", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    }]
+}
